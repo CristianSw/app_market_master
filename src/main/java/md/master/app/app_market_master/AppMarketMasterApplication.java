0817966f90cf197965603d66dcc896a3f8660673
CristianSw <60692725+CristianSw@users.noreply.github.com>
2023-03-27 19:02:00 +0300

    Init Commit

---
+++ src/main/java/md/master/app/app_market_master/AppMarketMasterApplication.java
@@ -0,0 +1,13 @@
+package md.master.app.app_market_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMarketMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMarketMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/md/master/app/app_market_master/AppMarketMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package md.master.app.app_market_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMarketMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
