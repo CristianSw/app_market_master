4943840c0baf6f08d7136b58eb8b08f6112c3377
CristianSw <60692725+CristianSw@users.noreply.github.com>
2023-04-17 11:41:07 +0300

    Base cart functionality added.

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,39 +1,54 @@
-create table products(id bigserial primary key , title varchar(255), price int);
-insert into products(title, price)
-values
-    ('Milk', 80),('Bread', 25),('Cheese', 500);
+create table products
+(
+    id    bigserial primary key,
+    title varchar(255),
+    price int
+);
 
 create table users
 (
-    id  bigserial primary key ,
-    username    varchar(36) not null,
-    password    varchar(80) not null
+    id       bigserial primary key,
+    username varchar(36) not null,
+    password varchar(80) not null
 );
 
 create table roles
 (
-    id  bigserial primary key ,
-    name    varchar(36) not null
+    id   bigserial primary key,
+    name varchar(50) not null
 );
 
 create table users_roles
 (
-    user_id bigint not null references users(id),
-    role_id bigint not null references roles(id),
-    primary key (user_id,role_id)
+    user_id bigint not null references users (id),
+    role_id bigint not null references roles (id),
+    primary key (user_id, role_id)
 );
 
-insert into roles(name)
-    values
-    ('ROLE_ADMIN'),
-    ('ROLE_USER');
-
+insert into roles (name)
+values ('ROLE_USER'),
+       ('ROLE_ADMIN');
 
 insert into users (username, password)
 values ('bob', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i'),
        ('john', '$2a$04$Fx/SX9.BAvtPlMyIIqqFx.hLY2Xp8nnhpzvEEVINvVpwIPbA3v/.i');
 
 insert into users_roles (user_id, role_id)
-VALUES
-    (1,1),
-    (2,2);
+VALUES (1, 1),
+       (2, 2);
+
+insert into products (title, price)
+values ('HDD', 2500),
+       ('SSD', 3500),
+       ('Ryzen3', 12000),
+       ('Ryzen5', 25000),
+       ('Ryzen7', 35000),
+       ('Ryzen9', 120000),
+       ('Flash Disk', 1500),
+       ('Flash microSD', 1000),
+       ('LCD монитор', 12000),
+       ('GeForce RTX2070', 75000),
+       ('Mouse', 350),
+       ('Keyboard', 1000);
+
+
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,37 +1,41 @@
 angular.module('market', []).controller('indexController', function ($scope, $http) {
-    $scope.loadProducts = function () {
-        $http.get('http://localhost:8189/market/api/v1/products').then(function (response) {
-            $scope.productsList = response.data;
-            // console.log(response);
-        });
+    $scope.fillTable = function () {
+        $http.get('http://localhost:8189/market/api/v1/products')
+            .then(function (response) {
+                $scope.products = response.data;
+            });
     };
-    //
-    $scope.showProductInfo = function (productId){
-        $http.get("http://localhost:8189/market/api/v1/products/" + productId).then(function (response){
-            alert(response.data.title);
-        })
+
+    $scope.fillCart = function () {
+        $http.get('http://localhost:8189/market/cart')
+            .then(function (response) {
+                $scope.cart = response.data;
+            });
+    };
+
+    $scope.deleteProduct = function (id) {
+        $http.delete('http://localhost:8189/market/api/v1/products/' + id)
+            .then(function (response) {
+                $scope.fillTable();
+            });
     }
 
-    $scope.deleteProductById = function (id) {
-        $http.delete('http://localhost:8189/market/api/v1/products/' + id).then(function (response) {
-            $scope.loadProducts();
-        });
+    $scope.addProductToCart = function (id) {
+        $http.get('http://localhost:8189/market/cart/' + id)
+            .then(function (response) {
+                $scope.fillCart();
+            });
     }
 
-    // $scope.loadCart = function () {
-    //     $http.get('http://localhost:8189/market/api/v1/cart').then(function (response) {
-    //         $scope.cart = response.data;
-    //         // console.log(response);
-    //     });
-    // }
-    //
-    // $scope.addToCart = function (productId) {
-    //     $http.get('http://localhost:8189/market/api/v1/cart/add/' + productId).then(function (response) {
-    //         $scope.loadCart();
-    //     });
-    // }
+    $scope.createNewProduct = function () {
+        // console.log($scope.newProduct);
+        $http.post('http://localhost:8189/market/api/v1/products', $scope.newProduct)
+            .then(function (response) {
+                $scope.newProduct = null;
+                $scope.fillTable();
+            });
+    }
 
-
-    $scope.loadProducts();
-    // $scope.loadCart();
+    $scope.fillTable();
+    $scope.fillCart();
 });
