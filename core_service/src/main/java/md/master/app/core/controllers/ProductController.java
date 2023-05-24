56a06f5ec8a6f07b3511234ea9f194b12c3d638d
cristian <cristian.dolinta@tutanota.com>
2023-05-24 20:00:32 +0300

    added pagination and filters for products, only working in api, front not ready yet. To be fixed when main task would be done as it's visual front modification.

--- core_service/src/main/java/md/master/app/core/controllers/ProductController.java
+++ core_service/src/main/java/md/master/app/core/controllers/ProductController.java
@@ -4,7 +4,9 @@ import md.master.app.api.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import md.master.app.api.ProductDto;
 import md.master.app.core.convertors.ProductConverter;
+import md.master.app.core.entities.Product;
 import md.master.app.core.services.ProductService;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,8 +22,18 @@ public class ProductController {
 
 
     @GetMapping
-    public List<ProductDto> findAllProducts() {
-        return productService.findAll().stream().map(productConverter::entityToDto ).collect(Collectors.toList());
+    public List<ProductDto> findProducts(
+            @RequestParam(required = false, name = "min_price") Integer min_price,
+            @RequestParam(required = false, name = "max_price") Integer max_price,
+            @RequestParam(required = false, name = "title") String title,
+            @RequestParam(defaultValue = "1", name = "p") Integer page
+    ) {
+        if (page < 1){
+            page = 1;
+        }
+        Specification<Product> spec = productService.createSpecByFilters(min_price,max_price,title);
+
+        return productService.findAll(spec,page- 1).map(productConverter::entityToDto).getContent();
     }
 
     @GetMapping("/{id}")
